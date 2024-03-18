95f64b523d6a83cef5923933328b5242a8cdea80
Sahatsawattankt <sahatsawatt.ankt@outlook.com>
2024-03-18 22:49:42 +0700

    Add DarkMode

--- resources/js/app.js
+++ resources/js/app.js
@@ -12,6 +12,10 @@ import "element-plus/dist/index.css";
 import VueSweetalert2 from "vue-sweetalert2";
 import "sweetalert2/dist/sweetalert2.min.css";
 
+import { useDark } from "@vueUse/core";
+
+const isDark = useDark();
+
 const appName = import.meta.env.VITE_APP_NAME || "Laravel";
 
 createInertiaApp({
