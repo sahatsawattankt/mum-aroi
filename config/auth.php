99f4de725950dc6ca50cc55ce48330e856b88815
Sahatsawattankt <sahatsawatt.ankt@outlook.com>
2024-03-19 15:31:30 +0700

    Add guards, Model, Migration, and Controller

--- config/auth.php
+++ config/auth.php
@@ -40,6 +40,14 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+        'admin' => [
+            'driver' => 'session',
+            'provider' => 'admins',
+        ],
+        'member' => [
+            'driver' => 'session',
+            'provider' => 'members',
+        ],
     ],
 
     /*
@@ -64,6 +72,18 @@ return [
             'driver' => 'eloquent',
             'model' => env('AUTH_MODEL', App\Models\User::class),
         ],
+
+        'admins' => [
+            'driver' => 'eloquent',
+            'model' => env('AUTH_MODEL', App\Models\Admin::class),
+        ],
+
+        'members' => [
+            'driver' => 'eloquent',
+            'model' => env('AUTH_MODEL', App\Models\Member::class),
+        ],
+
+
 
         // 'users' => [
         //     'driver' => 'database',
